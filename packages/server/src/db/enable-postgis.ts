5ff13df1d56702ee6ab8efd7fd915da8b75b8e5b
Dharm Patel <dharmpatel311287@gmail.com>
2026-03-01 15:38:04 +0530

    Update enable-postgis.ts

--- packages/server/src/db/enable-postgis.ts
+++ packages/server/src/db/enable-postgis.ts
@@ -2,10 +2,13 @@ import { db } from "./index";
 import { sql } from "drizzle-orm";
 
 const enablePostGIS = async () => {
-    console.log("Enabling PostGIS extension...");
+    console.log("Enabling PostGIS extension and optimizations...");
     try {
         await db.execute(sql`CREATE EXTENSION IF NOT EXISTS postgis`);
         console.log("✅ PostGIS enabled.");
+
+        await db.execute(sql`CREATE INDEX IF NOT EXISTS "chat_rooms_gix" ON "chatRooms" USING GIST (location)`);
+        console.log("✅ Spatial GIST Index created.");
     } catch (e) {
         console.error("❌ Failed to enable PostGIS", e);
     } finally {
